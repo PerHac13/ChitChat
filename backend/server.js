95f4a457b05b47564cbc6320431b905d0715a150
Shaikh_Abdullah <abdullah13112004@gmail.com>
2024-02-12 20:43:01 +0530

    Add: SinIn controller

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 
-// Routes
+// Files import
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-dotenv.config();
+// variable
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+dotenv.config();
 
+// Middleware
+app.use(express.json()); // to parse the incoming request with JSON payload (from req.body)
 app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hi from Backend!!");
+});
 
 app.listen(PORT, () => {
   connectToMongoDB();
